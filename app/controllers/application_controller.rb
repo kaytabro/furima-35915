240fb336e26a427c1c972af9c83e9968dd23afaf
kaytabro <kaytabro@gmail.com>
2021-09-20 12:24:12 +0900

    ユーザーの登録のビューを編集

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
   private
@@ -9,6 +10,10 @@ class ApplicationController < ActionController::Base
     # 環境変数を読み込む記述に変更
       username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday ])
   end
 
 end
