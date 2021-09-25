9dbae6a1c9156af1b83cd921d96d7f0921a67ebf
kaytabro <kaytabro@gmail.com>
2021-09-25 15:28:08 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,20 @@
 class Item < ApplicationRecord
 
   belongs_to :user
-  has_one_attached :image
 
+  #Active ssStorageとのアソシエーション
+  has_one_attached :image
+  
+  #Active Hashとのアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :take_days
+
+  #Active Hashのバリデーション
+  validates :category_id, :status_id, :postage_id, :prefecture_id, :take_days_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 
 end
