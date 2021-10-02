0c1160ace7128c08e747216bd61d7fd0d5f52aeb
kaytabro <kaytabro@gmail.com>
2021-10-02 18:43:00 +0900

    j自身の出品でないとトップページに戻される

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [ :new , :edit ]
+  before_action :move_to_index, only: [ :edit ]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -43,4 +44,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :explain, :category_id, :status_id, :postage_id, :prefecture_id, :take_days_id,
                                  :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    unless current_user.id == Item.find(params[:id]).user_id
+      redirect_to action: :index
+    end
+  end
+
 end
