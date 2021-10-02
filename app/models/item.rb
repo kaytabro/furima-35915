3e52d6a7c5f7ae5d44835b16d9de3c26e68cd967
kaytabro <kaytabro@gmail.com>
2021-10-02 16:22:56 +0900

    ユーザーのログイン状況と出品者本人のリクエストかの判定をして表示を分ける

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,10 +16,8 @@ class Item < ApplicationRecord
   # バリデーション
   validates :image, :name, :explain, :price, presence: true
   validates :price,
-            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999,
-                            message: 'は、半角数字で¥300~¥9,999,999の間で入力して下さい' }
+            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'は、半角数字で¥300~¥9,999,999の間で入力して下さい' }
 
   # Active Hashのバリデーション
-  validates :category_id, :status_id, :postage_id, :prefecture_id, :take_days_id, presence: true,
-                                                                                  numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, :status_id, :postage_id, :prefecture_id, :take_days_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
 end
