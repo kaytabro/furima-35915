2e72c60f30df94e9e1f6859fda1a4958a7ad18c9
kaytabro <kaytabro@gmail.com>
2021-10-02 19:52:00 +0900

    プルリクエストコメント対応（修正）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,13 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, only: [ :new, :edit ]
+  before_action :set_item, only: [ :show, :edit, :update ] 
+  before_action :move_to_index, only: [ :edit ]
 
   def index
     @items = Item.all.order('created_at DESC')
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def new
@@ -24,17 +24,17 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_path(@item)
     else
       render :edit
     end
   end
+
+
 
   private
 
@@ -43,7 +43,12 @@ class ItemsController < ApplicationController
                                  :price).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
     redirect_to action: :index unless current_user.id == Item.find(params[:id]).user_id
   end
+  
 end
