923ec7ff33e5448438c477395e2ccb76273f9324
kaytabro <kaytabro@gmail.com>
2021-10-02 14:05:18 +0900

    何もしていないのと同じ

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,9 +2,9 @@ class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
 
-  #def index
-    #@items = Item.all
-  #end
+  def index
+    @items = Item.all
+  end
 
   def new
     @item = Item.new
