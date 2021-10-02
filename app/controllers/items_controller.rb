ef768202fee12253aa9b7ed3fd9489c5bc1ab4a3
kaytabro <kaytabro@gmail.com>
2021-10-02 20:13:28 +0900

    コントローラーの修正とlink_toメソッドの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [ :new, :edit, :update, :destroy ]
+  before_action :authenticate_user!, only: [ :new, :edit ]
   before_action :set_item, only: [ :show, :edit, :update, :destroy ] 
-  before_action :move_to_index, only: [ :edit, :update, :destroy ]
+  before_action :move_to_index, only: [ :edit, :update ]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,8 +35,10 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if user_signed_in? && @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    end
   end
 
 
