29ec60be0f2d11d2feb64a649aec0a15d15793e9
kaytabro <kaytabro@gmail.com>
2021-10-09 16:48:36 +0900

    フォームオブジェクトでの保存を可能にする

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -4,5 +4,20 @@ class BuysController < ApplicationController
     @buy_add = BuyAdd.new
   end
 
-  
+  def create
+    @buy_add = BuyAdd.new(buy_params)
+    if @buy_add.valid?
+      @buy_add.save
+      redirect_to root_path
+    else
+      binding.pry
+      render :index
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_add).permit(:post_number, :prefecture_id, :municipalities, :address, :building, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id)
+  end
 end
--- app/models/buy_add.rb
+++ app/models/buy_add.rb
@@ -7,7 +7,7 @@ class BuyAdd
     validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipalities, :address
-    validates :phone_number, format: {with: /\A[0-9]{9}\z/, message: "is invalid. Not include hyphen(-)"}
+    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "is invalid. Not include hyphen(-)"}
     validates :item_id, :user_id
   end
 
@@ -15,7 +15,7 @@ class BuyAdd
     # 購入者情報を保存し、変数buyに代入する
     buy = Buy.create(item_id: item_id, user_id: user_id)
     # 住所を保存する,buy_idには、変数buyのidと指定する
-    Add.create(post_number: post_number, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, buy_id: buy.id)
+    Add.create(post_number: post_number, prefecture_id: prefecture_id, municipalities: municipalities, address: address, building: building, phone_number: phone_number, buy_id: buy.id)
   end
 
 end
