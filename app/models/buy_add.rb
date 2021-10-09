fec6e68374ce1a043bdb0698f76af28eaa6c7665
kaytabro <kaytabro@gmail.com>
2021-10-09 14:47:42 +0900

    フォームオブジェクトの設計

---
+++ app/models/buy_add.rb
@@ -0,0 +1,21 @@
+class BuyAdd
+
+  include ActiveModel::Model
+  attr_accessor :post_number, :prefecture, :municipalities, :address, :building, :phone_number, :item_id, :user_id
+
+  with_options presence: true do
+    validates :post_number, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture, numericality: { other_than: 1, message: "can't be blank" }
+    validates :municipalities, :address
+    validates :phone_number, format: {with: /\A[0-9]{9}\z/, message: "is invalid. Not include hyphen(-)"}
+    validates :item_id, :user_id
+  end
+
+  def save
+    # 購入者情報を保存し、変数buyに代入する
+    buy = Buy.create(item_id: item_id, user_id: user_id)
+    # 住所を保存する,buy_idには、変数buyのidと指定する
+    Add.create(post_number: post_number, prefecture: prefecture, municipalities: municipalities, address: address, building: building, buy_id: buy.id)
+  end
+
+end
